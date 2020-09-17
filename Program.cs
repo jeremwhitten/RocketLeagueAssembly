168e7a3a7fde9b00b4d2107402db31e8352c0d20
Poptart <70210614+PoptartWeScript@users.noreply.github.com>
2020-09-16 21:07:18 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -142,7 +142,7 @@ namespace RocketLeague
         static void Main(string[] args)
         {
             Console.WriteLine("WeScript.app RocketLeague Assembly By Poptart && GameHackerPM 0.1.6 BETA Loaded!");
-            bool returnedbool1 = WeScript.SDK.Utils.VIP.IsTopicContentUnlocked("/191-rocket-league-beta-v016/");
+            bool returnedbool1 = WeScript.SDK.Utils.VIP.IsTopicContentUnlocked("/191-rocket-league-beta-v017/");
 
             if(returnedbool1 == true)
             {
